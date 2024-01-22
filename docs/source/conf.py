3f2a503437cbc924bff3b708847eb737c2caaa91
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2024-01-22 14:43:36 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx_tabs.tabs',
+    'sphinx_copybutton',
 ]
 
 intersphinx_mapping = {
