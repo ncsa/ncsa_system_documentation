034e866edcea4a7e897527bad5a4bac4995473da
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2024-03-21 13:18:35 -0500

    Update custom.js

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -14,5 +14,15 @@ $( document ).ready(function() {
   // Next to the default "Edit on GitHub", add a separator, then the hub link.
   aside.prepend(separator);
   aside.prepend(hub_link);
-                    
+
+
+  // Insert "Documentation Hub" below html_logo in sidebar navigation
+  var docshub_link = document.createElement("a");
+  var docshub_link_text = document.createTextNode("Documentation Hub");
+  docshub_link.appendChild(docshub_link_text);
+  docshub_link.setAttribute("href","https://docs.ncsa.illinois.edu");
+  
+  wysidenavsearch = document.querySelector("body > div > nav > div > div.wy-side-nav-search > a");
+  wysidenavsearch.appendChild(docshub_link);
+  
 });
